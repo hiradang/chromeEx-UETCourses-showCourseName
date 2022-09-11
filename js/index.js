825e001d4b030fd51a0ff97df48dddc81591fd2d
hiradang <sunnybinhdang@gmail.com>
2022-09-11 11:58:37 +0700

    Show in EN page & create Popup Menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 $("document").ready(() => {
   // Get the ul that is the parent of courseName list
-
   let listCourses = $(
     '#block-region-side-post p:contains("Các khoá học của tôi")~ul'
   );
+
+  // Display in English page
+  if (listCourses.children().length == 0) {
+    listCourses = $('#block-region-side-post p:contains("My courses")~ul');
+  }
 
   listCourses.children().each((index, element) => {
     // get the a tag that contains the course's name
@@ -12,3 +16,11 @@ $("document").ready(() => {
     aTag.html(aTag.attr("title"));
   });
 });
+
+/*
+  Todo:
+  1. Tạo giao diện cho pop up -> Có thể bật tắt chức năng
+  2. Làm file README.md -> mô tả lại extension
+  3. Up lên chrome.
+
+*/
---
+++ js/popup.js
@@ -0,0 +1,4 @@
+$("#button").click(function () {
+  var button = $(this).val();
+  console.log(button);
+});
